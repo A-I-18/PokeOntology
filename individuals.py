9f5abc700f9f820c22fec1c0b1737c373e0358a3
ai18 <antonio.ionta.18@gmail.com>
2021-05-07 16:00:57 +0200

    Adding individuals

--- individuals.py
+++ individuals.py
@@ -7,6 +7,7 @@ def addInstances(onto):
     pikachu = onto.Pikachu()
     charmeleon = onto.Charmeleon()
     charizard = onto.Charizard()
+    charizard2 = onto.Charizard()
     bulbasaur = onto.Bulbasaur()
     wartortle = onto.Wartortle()
     zapdos = onto.Zapdos()
@@ -18,10 +19,13 @@ def addInstances(onto):
     pb2 = onto.NormalPokeBall()
     pb3 = onto.GreatPokeBall()
     pb4 = onto.GreatPokeBall()
+    pb5 = onto.PokeBall()
+    pb6 = onto.PokeBall()
 
     battle = onto.PokemonBattle("Ash_vs_Gary")
     f1 = onto.PokemonFight("wartortle_vs_evee")
     f2 = onto.PokemonFight("pikachu_vs_zapdos")
+    f3 = onto.PokemonFight("charizard_vs_charizard")
 
     '''
     PROPERTY ASSERTIONS
@@ -32,11 +36,13 @@ def addInstances(onto):
 
     pb1.contain.append(pikachu)
     pb2.contain.append(wartortle)
-    pb3.contain.append(evee)
-    pb4.contain.append(zapdos)
+    pb3.contain.append(charizard)
+    pb4.contain.append(evee)
+    pb5.contain.append(zapdos)
+    pb6.contain.append(charizard2)
 
-    ash.possess = [pb1, pb2]
-    gary.possess = [pb3, pb4, p1]
+    ash.possess = [pb1, pb2, pb3]
+    gary.possess = [pb4, pb5, pb6]
 
     ash.participateInBattle.append(battle)
     gary.participateInBattle.append(battle)
@@ -45,5 +51,14 @@ def addInstances(onto):
     evee.participateInFight.append(f1)
     pikachu.participateInFight.append(f2)
     zapdos.participateInFight.append(f2)
+    charizard.participateInFight.append(f3)
+    charizard2.participateInFight.append(f3)
 
-    battle.involves = [f1, f2]
+    wartortle.power = 50
+    evee.power = 65
+    pikachu.power = 105
+    zapdos.power = 100
+    charizard.power = 101
+    charizard2.power = 95
+
+    battle.involves = [f1, f2, f3]
